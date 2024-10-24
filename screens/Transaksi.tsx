eaf6e4f6cc0fc237f6850839e81c026579afac02
Achxter <hansphilemon@gmail.com>
2024-10-24 10:48:25 +0700

    added no data transaction

--- screens/Transaksi.tsx
+++ screens/Transaksi.tsx
@@ -1,7 +1,7 @@
 import { useFocusEffect } from '@react-navigation/native';
 import React from 'react';
-import { BackHandler, FlatList, Text, View } from 'react-native';
-import { Card, useTheme } from 'react-native-paper';
+import { BackHandler, FlatList, Text, View, ImageBackground, Image } from 'react-native';
+import { Card, Icon, useTheme } from 'react-native-paper';
 import { useSelector } from 'react-redux';
 
 interface HistoryItem {
@@ -36,31 +36,40 @@ function Transaksi({ navigation }) {
 
   return (
     <View style={{ height: '100%', backgroundColor: theme.colors.background }}>
-      <FlatList
-        data={history}
-        keyExtractor={(item) => item.transactionID}
-        renderItem={({ item }) => (
-          <Card
-            style={{ backgroundColor: theme.colors.secondaryContainer, padding: 16, paddingHorizontal: 24, marginBottom: 10 }}
-            onPress={() => navigation.navigate('DetailTransaksi', { item })}
-          >
-            <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-              <View>
-                <Text style={{ fontSize: 16, color: theme.colors.onSecondaryContainer }}>{item.transactionID}</Text>
-                <Text style={{ fontSize: 16, color: theme.colors.onSecondaryContainer }}>{item.merchant.toUpperCase()}</Text>
-                <Text style={{ fontSize: 16, color: item.status ? '#52b788' : 'red' }}>
-                  {item.status ? 'Berhasil' : 'Gagal'}
-                </Text>
+      {history.length === 0 ? (
+        <View style={{ height: '100%', backgroundColor: theme.colors.background, alignItems: 'center', justifyContent: 'center' }}>
+          <Icon size={50} source={() => (<Image style={{ width: 300, height: 300, borderRadius: 150, resizeMode: 'center', backgroundColor: theme.colors.surfaceVariant }} source={require('../assets/no-data.png')} />)} />
+          <Text style={{ fontSize: 20, marginTop: 50, textAlign: 'center', color: theme.colors.onBackground }}>
+            Tidak ada data transaksi
+          </Text>
+        </View>
+      ) :
+        <FlatList
+          data={history}
+          keyExtractor={(item) => item.transactionID}
+          renderItem={({ item }) => (
+            <Card
+              style={{ backgroundColor: theme.colors.secondaryContainer, padding: 16, paddingHorizontal: 24, marginBottom: 10 }}
+              onPress={() => navigation.navigate('DetailTransaksi', { item })}
+            >
+              <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                <View>
+                  <Text style={{ fontSize: 16, color: theme.colors.onSecondaryContainer }}>{item.transactionID}</Text>
+                  <Text style={{ fontSize: 16, color: theme.colors.onSecondaryContainer }}>{item.merchant.toUpperCase()}</Text>
+                  <Text style={{ fontSize: 16, color: item.status ? '#52b788' : 'red' }}>
+                    {item.status ? 'Berhasil' : 'Gagal'}
+                  </Text>
+                </View>
+                <View>
+                  <Text style={{ fontSize: 16, color: theme.colors.onSecondaryContainer, textAlign: 'right' }}>{item.date}</Text>
+                  <Text style={{ fontSize: 16, color: theme.colors.onSecondaryContainer, textAlign: 'right' }}>{item.nominal.toLocaleString('id-ID', { style: 'currency', currency: 'IDR' })}</Text>
+                </View>
               </View>
-              <View>
-                <Text style={{ fontSize: 16, color: theme.colors.onSecondaryContainer, textAlign: 'right' }}>{item.date}</Text>
-                <Text style={{ fontSize: 16, color: theme.colors.onSecondaryContainer, textAlign: 'right' }}>{item.nominal.toLocaleString('id-ID', { style: 'currency', currency: 'IDR' })}</Text>
-              </View>
-            </View>
-          </Card>
-        )}
-        contentContainerStyle={{ paddingHorizontal: 24, paddingTop: 16 }}
-      />
+            </Card>
+          )}
+          contentContainerStyle={{ paddingHorizontal: 24, paddingTop: 16 }}
+        />
+      }
     </View>
   );
 }
